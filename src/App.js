360eafc739e8cb5103704426eebfca29a37e4e64
Vish7284 <vp75240194465@gmail.com>
2024-04-19 16:58:31 +0530

    fetchApi

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React ,{useState}from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies,setMovies]=useState([]);
+  async function fetchMovieHandler(){
+    const respose = await fetch("https://swapi.dev/api/films")
+    const data = await respose.json();
+      const transformedMovies = data.results.map((movieData)=>{
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      })
+      setMovies(transformedMovies)
+    
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
