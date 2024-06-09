275ea38f266af3ddc031f3cc318299ec020365db
Vish7284 <vp75240194465@gmail.com>
2024-06-09 14:54:20 +0530

    Nextjs1stComit

---
+++ pages/aboutus.js
@@ -0,0 +1,8 @@
+import { deflate } from "zlib";
+
+
+const AboutUs = ()=>{
+    return(<p>Welocme to About uS
+    </p>)
+};
+export default AboutUs;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+
+// pages/index.js
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
