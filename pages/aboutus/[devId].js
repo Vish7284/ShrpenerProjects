2d35d79d66f6c7a255599f9ae5de19015ab79a02
Vish7284 <vp75240194465@gmail.com>
2024-06-10 08:54:45 +0530

    develpoer not exist

--- pages/aboutus/[devId].js
+++ pages/aboutus/[devId].js
@@ -1,27 +1,40 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const Developer =()=>{
-    const router = useRouter();
-    const devId = router.query.devId;
-     const details = [
-       { id: 1, name: "Yash", role: "Senior Developer" },
+const Developer = () => {
+  const router = useRouter();
+  const { devId } = router.query;
 
-       { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  const [developer, setDeveloper] = useState(null);
 
-       { id: 3, name: "Suresh", role: "Frontend Developer" },
-     ];
-     const filterData = details.filter((dev)=> dev.id == devId)
-    
-    return(
-        <>
-        {filterData.map((dev)=> {
-            return(<div key={dev.id}>
-                <h1>{dev.name}</h1>
-                <h3>{dev.role}</h3>
-                </div>
-            )
-        })}
-        </>
-    )
+  useEffect(() => {
+    if (devId) {
+      const details = [
+        { id: 1, name: "Yash", role: "Senior Developer" },
+        { id: 2, name: "Vaibhav", role: "Backend Developer" },
+        { id: 3, name: "Suresh", role: "Frontend Developer" },
+      ];
+      const filterData = details.filter((dev) => dev.id == devId);
+      setDeveloper(filterData[0]);
+    }
+  }, [devId]);
+
+  if (!devId) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {developer ? (
+        <div key={developer.id}>
+          <h1>{developer.name}</h1>
+          <h3>{developer.role}</h3>
+        </div>
+      ) : (
+        <p>Developer doesn't exist</p>
+      )}
+    </>
+  );
 };
+
 export default Developer;
