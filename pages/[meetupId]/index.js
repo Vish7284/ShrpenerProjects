0ff93131719dd612f4962938cff181ff42382271
Vish7284 <vp75240194465@gmail.com>
2024-06-10 12:39:02 +0530

    just

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 
 
-const MeetDetails =()=>{
+const MeetDetails =(props)=>{
     return(
         <>
         <img src="https://www.crafttrip.in/image/cache/catalog/radha-krishna/radha-krishna-painting-with-peacock-feather-and-krishna-flute-800x534w.jpeg" alt="First meet"/>
