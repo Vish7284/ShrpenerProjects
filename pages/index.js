f9308c5bc6144300d308c72c7cf9b80a7b878870
Vish7284 <vp75240194465@gmail.com>
2024-06-11 14:07:55 +0530

    backendMongoDb

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
-
+import { title } from "process";
 
 const DummyMeetup = [
   {
@@ -9,7 +9,7 @@ const DummyMeetup = [
     image:
       "https://i.pinimg.com/originals/7e/76/11/7e7611330ca1c2bc121e14bb594f9a7c.jpg",
     address: "xyz cafe",
-    description:"The Last Meet",
+    description: "The Last Meet",
   },
   {
     id: "m2",
@@ -17,18 +17,17 @@ const DummyMeetup = [
     image:
       "https://www.crafttrip.in/image/cache/catalog/radha-krishna/radha-krishna-painting-with-peacock-feather-and-krishna-flute-800x534w.jpeg",
     address: "xyz && luv cafe",
-    description:"The Last Meet",
+    description: "The Last Meet",
   },
 ];
 const HomePage = (props) => {
-// const [loadesMeet , setLoadedMeet] = useState([])
-//     useEffect(()=>{
-//         setLoadedMeet(DummyMeetup)
-//     },[])
+  // const [loadesMeet , setLoadedMeet] = useState([])
+  //     useEffect(()=>{
+  //         setLoadedMeet(DummyMeetup)
+  //     },[])
 
   return <MeetupList meetups={props.meetups} />;
 };
-
 
 // export async function getServerSideProps(context) {
 
@@ -41,12 +40,26 @@ const HomePage = (props) => {
 //   };
 // }
 export async function getStaticProps() {
-    return({
-        props:{
-            meetups :DummyMeetup,
-        },
-        revalidate : 1,
-})
+  const client = await MongoClient.connect(
+    "mongodb+srv://vishal:4eTKheKtErJmIBQd@cluster0.f8itl6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupCollection = db.collection("meetups");
+const meetups = await meetupCollection.find().toArray();
+
+client.close()
+  return {
+    props: {
+      meetups: meetups.map(meet=> ({
+        id : meet._id.toString(),
+        title:meet.title,
+        address:meet.address,
+        description:meet.description,
+        image:meet.image,
+      })),
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,20 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
-
+import { useRouter } from "next/router";
 const NewMeetUp =()=>{
-    const addingMeetup =(meetUpdata)=>{
-console.log(meetUpdata);
+    const router = useRouter();
+    const addingMeetup =async(meetUpdata)=>{
+        const response = await fetch("/api/new-meetup",{
+            method:"POST",
+            body:JSON.stringify(meetUpdata),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+
+        router.push("/");
+
     }
     return <NewMeetupForm onAddMeetup ={addingMeetup}/>;
 };
